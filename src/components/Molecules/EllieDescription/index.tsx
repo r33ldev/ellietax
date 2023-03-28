f05d118a4b235fd70a6e1a54f62778f998d7774e
Michael E Adebisi <adebisienads@gmail.com>
2023-03-28 18:59:41 +0100

    ui fixes
    Signed-off-by: Michael E Adebisi <adebisienads@gmail.com>

--- src/components/Molecules/EllieDescription/index.tsx
+++ src/components/Molecules/EllieDescription/index.tsx
@@ -12,43 +12,40 @@ interface indexProps {}
 export const EllieDescription: React.FC<indexProps> = ({}) => {
   return (
     <Section>
-      <Flex
-        width="85%"
-        background="white"
-        margin="10rem auto"
-        borderRadius="64px"
-        justify="space-between"
-        // align="stretch"
-        gap="3rem"
+      <Section
+        styles={{
+          width: "85%",
+          margin: "10rem auto",
+          background: "white",
+          borderRadius: "64px",
+        }}
       >
-        <Section styles={{ padding: "6rem" }}>
-          <EllieDescriptionIcon />
-          <Text
-            type="p"
-            text="A full-service accounting firm in midtown Manhattan, our seasoned certified public accountants will remove your financial pain points with accounting services that make managing your money easier. We have the essential tools you need to track expenses, monitor cash flow, and identify financial trends so you can plan for the future of your business."
-            color="#494949"
-            fontSize="2rem"
-            styles={{ marginTop: "1rem", lineHeight: "3rem" }}
-          />
-          <Flex gap="2rem" margin="2rem 0">
-            <Image src={irsfile} alt="" width="150" height="50" />
-            <Image src={agent} alt="" width="150" height="50" />
-          </Flex>
-        </Section>
-        <Section styles={{width:'45%', height:'auto'}}>
+        <Flex align="center" gap="6rem" justify="space-between">
+          <Section styles={{ width: "60%", padding: "0 5rem" }}>
+            <EllieDescriptionIcon />
+            <Text
+              type="p"
+              text="A full-service accounting firm in midtown Manhattan, our seasoned certified public accountants will remove your financial pain points with accounting services that make managing your money easier. We have the essential tools you need to track expenses, monitor cash flow, and identify financial trends so you can plan for the future of your business."
+              color="#494949"
+              fontSize="2rem"
+              styles={{ marginTop: "1rem", lineHeight: "3rem" }}
+            />
+            <Flex gap="2rem" margin="2rem 0">
+              <Image src={irsfile} alt="" width="150" height="50" />
+              <Image src={agent} alt="" width="150" height="50" />
+            </Flex>
+          </Section>
           <Image
             src={womenImage}
-            alt=""
-            width={490}
-            //   height={600}
+            alt="customer support"
             style={{
-                //   width: "550px",
-            //   minHeight: "600px",
-            //   height: "50%",
+              width: "40%",
+              height: "100%",
+              maxWidth: "435px",
             }}
           />
-        </Section>
-      </Flex>
+        </Flex>
+      </Section>
     </Section>
   );
 };
