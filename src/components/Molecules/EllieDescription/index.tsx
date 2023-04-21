46b9e18608c5934b7ca420a835d97c284ab0cdc4
Michael E Adebisi <adebisienads@gmail.com>
2023-04-21 21:02:15 +0100

    EllieDescription mobile
    Signed-off-by: Michael E Adebisi <adebisienads@gmail.com>

--- src/components/Molecules/EllieDescription/index.tsx
+++ src/components/Molecules/EllieDescription/index.tsx
@@ -3,13 +3,19 @@ import Section from "@/components/Section";
 import Image from "next/image";
 import React from "react";
 import womenImage from "@/assets/images/portrait-women.png";
-import EllieDescriptionIcon from "@/components/icons/EllieDescription";
+import womenImageMb from "@/assets/images/portrait-women-mb.png";
+import {
+  EllieDescriptionIcon,
+  EllieDescriptionIconMobile,
+} from "@/components/icons/EllieDescription";
 import Text from "@/components/Text";
 import irsfile from "@/assets/images/irsfile.png";
 import agent from "@/assets/images/agent.png";
+import { useScreenResolution } from "@/hooks/useScreenResolution";
 interface indexProps {}
 
 export const EllieDescription: React.FC<indexProps> = ({}) => {
+  const { isMobile } = useScreenResolution();
   return (
     <Section>
       <Section
@@ -17,29 +23,58 @@ export const EllieDescription: React.FC<indexProps> = ({}) => {
           width: "85%",
           margin: "10rem auto",
           background: "white",
-          borderRadius: "64px",
+          borderRadius: isMobile ? "32px" : "64px",
         }}
       >
-        <Flex align="center" gap="6rem" justify="space-between">
-          <Section styles={{ width: "60%", padding: "0 5rem" }}>
-            <EllieDescriptionIcon />
+        <Flex
+          align="center"
+          gap={isMobile ? "3rem" : "6rem"}
+          justify="space-between"
+          direction={isMobile ? "column" : "row"}
+        >
+          <Section
+            styles={{
+              width: isMobile ? "100%" : "60%",
+              padding: `${isMobile ? "5rem 2.5rem 0rem" : "0 5rem"}`,
+            }}
+          >
+            {isMobile ? (
+              <EllieDescriptionIconMobile />
+            ) : (
+              <EllieDescriptionIcon />
+            )}
             <Text
               type="p"
               text="A full-service accounting firm in midtown Manhattan, our seasoned certified public accountants will remove your financial pain points with accounting services that make managing your money easier. We have the essential tools you need to track expenses, monitor cash flow, and identify financial trends so you can plan for the future of your business."
               color="#494949"
-              fontSize="2rem"
-              styles={{ marginTop: "1rem", lineHeight: "3rem" }}
+              fontSize={isMobile ? "1.6rem" : "2rem"}
+              styles={{
+                marginTop: isMobile ? "2.5rem" : "1rem",
+                lineHeight: "3rem",
+              }}
             />
-            <Flex gap="2rem" margin="2rem 0">
-              <Image src={irsfile} alt="" width="150" height="50" />
-              <Image src={agent} alt="" width="150" height="50" />
+            <Flex gap={`${isMobile ? ".5rem" : "2rem"}`} margin="2rem 0">
+              <Image
+                src={irsfile}
+                alt=""
+                width={isMobile ? "100" : "150"}
+                height="50"
+                style={{ objectFit: "scale-down" }}
+              />
+              <Image
+                style={{ objectFit: "scale-down" }}
+                src={agent}
+                alt=""
+                width={isMobile ? "100" : "150"}
+                height="50"
+              />
             </Flex>
           </Section>
           <Image
-            src={womenImage}
+            src={isMobile ? womenImageMb : womenImage}
             alt="customer support"
             style={{
-              width: "40%",
+              width: isMobile ? "100%" : "40%",
               height: "100%",
               maxWidth: "435px",
             }}
--- src/components/icons/EllieDescription.tsx
+++ src/components/icons/EllieDescription.tsx
@@ -45,4 +45,49 @@ export const EllieDescriptionIcon: React.FC<EllieDescriptionProps> = ({}) => {
     </svg>
   );
 };
-export default EllieDescriptionIcon;
+export const EllieDescriptionIconMobile: React.FC<EllieDescriptionProps> = ({}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="293"
+      height="171"
+      viewBox="0 0 293 171"
+    >
+      <g id="Group_356" data-name="Group 356" transform="translate(-19 -103)">
+        <text
+          id="Seasoned_certified_public_accounting_firm_and_tax_consultancy"
+          data-name="Seasoned certified public accounting firm and tax consultancy"
+          transform="translate(19 103)"
+          fill="#161616"
+          font-size="32"
+          font-family="Avenir-Heavy, Avenir"
+          font-weight="800"
+        >
+          <tspan x="0" y="32">
+            Seasoned certified{" "}
+          </tspan>
+          <tspan x="0" y="75">
+            public accounting{" "}
+          </tspan>
+          <tspan x="0" y="118">
+            firm and tax{" "}
+          </tspan>
+          <tspan x="0" y="161">
+            consultancy
+          </tspan>
+        </text>
+        <path
+          id="Path_61"
+          data-name="Path 61"
+          d="M249,824.659c72.607-7.869,115.433,0,115.433,0"
+          transform="translate(-71.173 -681.683)"
+          fill="none"
+          stroke="#4e7aef"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        />
+      </g>
+    </svg>
+  );
+}
