e5ae53eed35234ce3b779ad37b88c868d725b10a
Michael E Adebisi <adebisienads@gmail.com>
2023-04-23 00:33:24 +0100

    ewrdfgh
    Signed-off-by: Michael E Adebisi <adebisienads@gmail.com>

--- src/pages/drop-off/[id].tsx
+++ src/pages/drop-off/[id].tsx
@@ -4,8 +4,10 @@ import popp from "@/assets/images/popp.jpg";
 import trash from "@/assets/images/trash.jpg";
 import Button from "@/components/Button";
 import Flex from "@/components/Flex";
+import HeaderNav from "@/components/Molecules/Header/HeaderNav";
 import Section from "@/components/Section";
 import Text from "@/components/Text";
+import { useScreenResolution } from "@/hooks/useScreenResolution";
 import Applayout from "@/layout/AppLayout";
 import CloseIcon from "@mui/icons-material/Close";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
@@ -13,6 +15,7 @@ import QueryBuilderIcon from "@mui/icons-material/QueryBuilder";
 import { Box } from "@mui/material";
 import Modal from "@mui/material/Modal";
 import { styled } from "@mui/system";
+import { Popup } from "antd-mobile";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
@@ -58,6 +61,65 @@ const ImageWrapper = styled("div")<{ hover: boolean }>(({ hover }) => ({
   position: "relative",
 }));
 
+const ModalContent = ({
+  style,
+  handleClose,
+  isMobile,
+}: {
+  style: any;
+  handleClose: () => void;
+  isMobile: boolean;
+}) => {
+  return (
+    <Box sx={style}>
+      {!isMobile && (
+        <CloseIcon
+          onClick={handleClose}
+          fontSize="large"
+          style={{
+            position: "absolute",
+            top: "30px",
+            right: "30px",
+            cursor: "pointer",
+          }}
+        />
+      )}
+      <Image src={popp} alt="" width={257} />
+      <Text
+        text="Updated successfully"
+        type="h2"
+        fontSize={isMobile ? "2.4rem" : "4rem"}
+        weight="600"
+      />
+      <Text
+        text="Our commitment to integrity, professionalism, and customer satisfaction means that you can trust us to handle your taxes with the utmost care and confidentiality."
+        type="p"
+        fontSize={isMobile ? "1.6rem" : "2rem"}
+        weight="400"
+        color="#494949"
+        styles={{
+          marginTop: "2rem",
+          textAlign: "center",
+          lineHeight: "3rem",
+          width: "80%",
+        }}
+      />
+
+      <div style={{ display: "flex", alignItems: "center", margin: "2rem 0" }}>
+        <QueryBuilderIcon />
+        <Text
+          text="Response time: less than 24hrs"
+          type="p"
+          fontSize={isMobile ? "1.4rem" : "1.6rem"}
+          weight="800"
+          color="#494949"
+          styles={{ marginLeft: "1rem" }}
+        />
+      </div>
+    </Box>
+  );
+};
+
 export const NewDropoff: React.FC<indexProps> = ({}) => {
   const [open, setOpen] = useState(false);
   const router = useRouter();
@@ -71,83 +133,67 @@ export const NewDropoff: React.FC<indexProps> = ({}) => {
     handleOpen();
   }
   const style = {
-    position: "absolute" as "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    bgcolor: "white",
     borderRadius: "30px",
-    boxShadow: 24,
-    p: 4,
-    width: "750px",
     flexDirection: "column",
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
-    padding: "6rem 0",
+    fontFamily: "Avenir, sans-serif",
   };
+  const { isMobile } = useScreenResolution();
   return (
     <Applayout titleTag="Create new drop-off - Ellietax ">
-      <Modal open={open} onClose={handleClose}>
-        <Box sx={style}>
-          <CloseIcon
-            onClick={handleClose}
-            fontSize="large"
+      {isMobile ? (
+        <Popup
+          visible={open}
+          onClose={handleClose}
+          onMaskClick={handleClose}
+          bodyStyle={{
+            overflow: "scroll",
+            borderRadius: "16px 16px 0px 0px",
+            height: "60vh",
+          }}
+        >
+          <Pad onClick={handleClose} />
+          <ModalContent
+            style={style}
+            handleClose={handleClose}
+            isMobile={isMobile}
+          />
+        </Popup>
+      ) : (
+        <Modal open={open} onClose={handleClose}>
+          <ModalContent
             style={{
-              position: "absolute",
-              top: "30px",
-              right: "30px",
-              cursor: "pointer",
+              ...style,
+              position: "absolute" as "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              bgcolor: "white",
+              boxShadow: 24,
+              p: 4,
+              padding: "6rem 0",
+              width: "750px",
             }}
+            handleClose={handleClose}
+            isMobile={isMobile}
           />
-          <Image src={popp} alt="" width={257} />
-          <Text
-            text="Updated successfully"
-            type="h2"
-            fontSize="4rem"
-            weight="600"
-          />
-          <Text
-            text="Our commitment to integrity, professionalism, and customer satisfaction means that you can trust us to handle your taxes with the utmost care and confidentiality."
-            type="p"
-            fontSize="2rem"
-            weight="400"
-            color="#494949"
-            styles={{
-              marginTop: "2rem",
-              textAlign: "center",
-              lineHeight: "3rem",
-              width: "80%",
-            }}
-          />
-
-          <div
-            style={{ display: "flex", alignItems: "center", margin: "2rem 0" }}
-          >
-            <QueryBuilderIcon />
-            <Text
-              text="Response time: less than 24hrs"
-              type="p"
-              fontSize="1.6rem"
-              weight="800"
-              color="#494949"
-              styles={{ marginLeft: "1rem" }}
-            />
-          </div>
-        </Box>
-      </Modal>
+        </Modal>
+      )}
       <DropoffTypeWrapper>
+        {isMobile && <HeaderNav isMobile={isMobile} />}
         <Section
           styles={{
-            width: "45vw",
+            width: isMobile ? "85%" : "45vw",
             maxWidth: "620px",
-            margin: "7rem auto",
+            margin: `${isMobile ? "1rem" : "7rem"} auto`,
           }}
         >
           <Text
             text="Documents"
             type="h2"
-            fontSize="5.6rem"
+            fontSize={isMobile ? "4rem" : "5.6rem"}
             weight="600"
             styles={{ paddingTop: "1rem" }}
           />
@@ -156,16 +202,16 @@ export const NewDropoff: React.FC<indexProps> = ({}) => {
             <Text
               text="Update documents"
               type="h2"
-              fontSize="2rem"
+              fontSize={isMobile ? "1.6rem" : "2rem"}
               weight="600"
               styles={{ paddingTop: "1rem" }}
             />
-            <UploadArea>
+            <UploadArea isMobile={isMobile}>
               <Section>
                 <Image src={attachment} width="50" height="50" alt="" />
                 <p
                   style={{
-                    fontSize: "2rem",
+                    fontSize: { isMobile } ? "1.6rem" : "2rem",
                     fontWeight: "400",
                     color: "#494949",
                     margin: "1.2rem 0",
@@ -177,21 +223,35 @@ export const NewDropoff: React.FC<indexProps> = ({}) => {
                 <Text
                   text="PNG, JPG, PDF, GIF up to 10MB"
                   type="p"
-                  fontSize="1.6rem"
+                  fontSize={isMobile ? "1.4rem" : "1.6rem"}
                   weight="400"
                 />
               </Section>
             </UploadArea>
-            <Flex gap="2rem">
+            <GridMan isMobile={isMobile}>
               {Array.from({ length: 4 }).map((_, index) => (
                 <UploadedImage key={index} />
               ))}
-            </Flex>
+            </GridMan>
           </UploadDropoffs>
         </Section>
-        <Divider />
-        <div>
-          <Flex align="center" gap="5rem">
+        <div
+          style={{
+            width: "100%",
+            ...(isMobile && {
+              position: "fixed",
+              bottom: 0,
+              background: "white",
+            }),
+          }}
+        >
+          <Divider />
+          <Flex
+            align="center"
+            gap="5rem"
+            margin={`${isMobile ? 2 : 3}rem 0`}
+            justify="center"
+          >
             <Section>
               <Flex align="center">
                 <KeyboardArrowLeftIcon />
@@ -208,7 +268,7 @@ export const NewDropoff: React.FC<indexProps> = ({}) => {
             <Button
               text={"Update documents"}
               background="#4E7AEF"
-              width="283px"
+              width={isMobile ? "221px" : "283px"}
               height="50px"
               onSubmit={() => updateDocument()}
             />
@@ -232,35 +292,19 @@ const Divider = styled("div")(() => ({
   width: "100%",
   height: "1px",
   background: "#E5E5E5",
-  margin: "2rem 0 7rem 0",
-}));
-
-const DropoffTypes = styled("div")(() => ({
-  margin: "7rem 0 1rem 0",
-}));
-const DropoffType = styled("div")<{ border: string }>(({ border }) => ({
-  display: "flex",
-  alignItems: "center",
-  gap: "3rem",
-  align: "center",
-  margin: "3rem 0",
-  cursor: "pointer",
-  padding: "2rem",
-  border: border,
-  borderRadius: "10px",
-  //   transition: "all 0.3s ease-in-out",
-  maxWidth: "500px",
+  // marginTop: "2rem",
+  // paddingBottom: "7rem",
 }));
 
 const UploadDropoffs = styled("div")(() => ({
   margin: "2rem 0 1rem 0",
 }));
 
-const UploadArea = styled("div")(() => ({
+const UploadArea = styled("div")<{ isMobile: boolean }>(({ isMobile }) => ({
   width: "100%",
-  height: "250px",
+  height: isMobile ? "184px" : "250px",
   background: "transparent",
-  margin: "3rem 0",
+  margin: `${isMobile ? 2 : 3}rem 0`,
   borderRadius: "16px",
   border: "2px dashed #E7E7E7",
   display: "flex",
@@ -270,4 +314,17 @@ const UploadArea = styled("div")(() => ({
   cursor: "pointer",
 }));
 
+const Pad = styled("div")(() => ({
+  height: ".8rem",
+  width: "15rem",
+  backgroundColor: "#E7E7E7",
+  borderRadius: "10px",
+  margin: "2rem auto",
+}));
+const GridMan = styled("div")<{ isMobile: boolean }>(({ isMobile }) => ({
+  display: "grid",
+  gridTemplateColumns: `repeat(${isMobile ? 2 : 4}, minmax(160px, 1fr))`,
+  gridGap: "2rem",
+  overflow: "hidden",
+}));
 export default NewDropoff;
